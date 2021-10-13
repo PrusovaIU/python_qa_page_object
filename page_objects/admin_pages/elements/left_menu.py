5d6cfb4892290e7d75b5d61ffd525d4ca93fcbb8
Prusova Iulia <prusova.work@gmail.com>
2021-10-13 10:41:45 +0300

    New: test add new good. Has not been ready yet

--- page_objects/admin_pages/elements/left_menu.py
+++ page_objects/admin_pages/elements/left_menu.py
@@ -3,22 +3,33 @@ from page_objects.bases.CONSTS import LOCATOR_TYPE
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
+from typing import List
 
 
-class MenuBlock:
+class MenuBlock(BaseElement):
     TITLE_LOCATOR = (By.TAG_NAME, 'a')
     LIST_LOCATOR = (By.TAG_NAME, 'ul')
 
-    def __init__(self, browser: WebDriver, menu_block: WebElement):
-        self.__browser = browser
-        self.__menu_block = menu_block
+    def __init__(self, browser: WebDriver, parent: WebElement, locator: LOCATOR_TYPE):
+        super(MenuBlock, self).__init__(browser, locator, parent)
+        self.__list = BaseElement(self._browser, self.LIST_LOCATOR, self._self)
 
     @property
     def title(self) -> BaseElement:
-        return BaseElement(self.__browser, self.TITLE_LOCATOR, self.__menu_block)
+        return BaseElement(self._browser, self.TITLE_LOCATOR, self._self)
 
+    @property
     def list(self) -> BaseElement:
-        return BaseElement(self.__browser, self.LIST_LOCATOR, self.__menu_block)
+        return self.__list
+
+    def click_item(self, i: int):
+        """
+        Click to item of the menu
+        :param i: item's position in menu
+        :return: None
+        """
+        items: List[WebElement] = self.list.get_children()
+        items[i].click()
 
 
 class LeftMenu(BaseElement):
@@ -29,8 +40,7 @@ class LeftMenu(BaseElement):
         super(LeftMenu, self).__init__(browser, self.LOCATOR)
 
     def __get_menu_block(self, locator: LOCATOR_TYPE) -> MenuBlock:
-        menu_block = self.find_element(locator)
-        return MenuBlock(self._browser, menu_block)
+        return MenuBlock(self._browser, self._self, locator)
 
     @property
     def catalog(self) -> MenuBlock:
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,8 +1,14 @@
+from page_objects.admin_pages.bases.bases_admin_page import BaseAdminPage
 from page_objects.admin_pages.login_page import AdminLoginPage
 from time import sleep
 
 
 def test_add_new_good(browser, admin):
     AdminLoginPage(browser, f"{browser.current_url}/admin").login(admin[0], admin[1])
+    left_menu = BaseAdminPage(browser, browser.current_url, False).left_menu
+    left_menu.catalog.click()
+    left_menu.catalog.click_item(1)
+    sleep(1)
+
 
 
